22e3b3664c9fe0450b3c23f433a58b033026bd0f
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-03-11 13:45:15 +0100

    Initial commit

---
+++ src/main/java/com/example/dbdependencyinjection/DbDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDependencyInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbdependencyinjection/DbDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
